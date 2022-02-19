a643056e621bb27547dd4d7211daf2bc6863acba
Vykula <viktoria.zheneral@gmail.com>
2022-02-19 23:34:17 +0200

    StartGame() on button click added

--- Assets/Scripts/CanvasManager.cs
+++ Assets/Scripts/CanvasManager.cs
@@ -5,23 +5,26 @@ using UnityEngine.UI;
 
 public class CanvasManager : MonoBehaviour
 {
+    public GameObject menuUI;
+
     public Button startButton;
     public Button exitButton;
 
     void Start()
     {
+        Time.timeScale = 0;
         startButton.GetComponent<Button>().onClick.AddListener(StartGame);
         exitButton.GetComponent<Button>().onClick.AddListener(ExitGame);
     }
 
     void StartGame()
     {
-
+        menuUI.SetActive(false);
+        Time.timeScale = 1;
     }
 
     void ExitGame()
     {
-        Debug.Log("gg wp");
         Application.Quit();
     }
 }
